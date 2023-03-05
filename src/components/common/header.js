c7bbef5e2c2667739ea17bdc2af8a7ffa5d37231
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-05 14:46:06 +0900

    #1. Work on header and layout components

---
+++ src/components/common/header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Menu } from "antd";
+
+function AppHeader() {
+  return (
+    <div className="container-fluid">
+      <div className="header">
+        <div className="logo">
+          <i className="fas fa-bolt"></i>
+          <a href="http://www.google.com">Tech</a>
+        </div>
+        <Menu mode="horizontal" defaultSelectedKeys={["home"]}>
+          <Menu.Item key="home">Home</Menu.Item>
+          <Menu.Item key="about">About</Menu.Item>
+          <Menu.Item key="features">Features</Menu.Item>
+          <Menu.Item key="howitworks">Hot it works</Menu.Item>
+          <Menu.Item key="faq">FAQ</Menu.Item>
+          <Menu.Item key="pricing">Pricing</Menu.Item>
+          <Menu.Item key="contact">Contact</Menu.Item>
+        </Menu>
+      </div>
+    </div>
+  );
+}
+
+export default AppHeader;
